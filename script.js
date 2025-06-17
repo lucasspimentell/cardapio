d2168c43b2c18f5864c068686e3d3319874c626a
Lucas pimentel <lucas47b@hotmail.com>
2025-06-17 10:10:08 -0300

    atualizando 17/06

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function comprar(botao) { /*   função com parametro do botão para que o botão clicado seja acionado aqui e assim ativar as funçôes*/
 
-
      if (botao.value == "comprar") {
 
           botao.value = "Adicionado";
           botao.style.background = "limegreen";
-          if(botao.value == "Adicionado"){
+          /*if(botao.value == "Adicionado"){
              alert("Adionado ao carrinho");
-          }
+          }*/
 
 
      } else {
 
           botao.value = "comprar";
-          botao.style.background = " #eedf10";
+          botao.style.background = "#ffbc11";
+          botao.style.houver.background ="#f1f10b"
+
      }
-
-
 }
